935f3e8378f16f35c7d5c16f5d14b9bdc8bd12cc
Rapha-Y <raph.yuki@gmail.com>
2020-12-22 09:53:01 -0300

    add todo input component

---
+++ client/src/components/InputTodo.js
@@ -0,0 +1,42 @@
+import React, { Fragment, useState } from 'react';
+
+const InputTodo = () => {
+    const [description, setDescription] = useState('');
+
+    const onSubmitForm = async e => {
+        e.preventDefault();
+        try {
+            const body = { description };
+            await fetch(
+                'http://localhost:5000/todos',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(body)
+                }
+            );
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    return (
+        <Fragment>
+            <h1 className='text-center my-5'>Input Todo</h1>
+            <form className='d-flex' onSubmit={onSubmitForm}>
+                <input 
+                    type='text' 
+                    placeholder='add todo' 
+                    className='form-control' 
+                    value={description} 
+                    onChange={e => setDescription(e.target.value)}
+                />
+                <button className='btn btn-success'>Add</button>
+            </form>
+        </Fragment>
+    );
+}
+
+export default InputTodo;
--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const pool = require('./db');
 
 //Middleware
+app.use(cors());
+
 app.use(express.json());
 
 //Routes
