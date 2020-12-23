962bc4ede25730e422f99073ba3dd4d42ca67c37
Rapha-Y <raph.yuki@gmail.com>
2020-12-23 12:07:50 -0300

    add user login method

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -34,4 +34,32 @@ router.post('/register', async (req, res) => {
     }
 });
 
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await pool.query(
+            'SELECT * FROM users WHERE user_email = $1',
+            [email]
+        );
+
+        if (user.rows.length === 0) {
+            return res.status(401).send('No users are registered under this e-mail');
+        }
+
+        const validPassword = await bcrypt.compare(password, user.rows[0].user_password);
+
+        if (!validPassword) {
+            return res.status(401).send('Incorrect password');
+        }
+
+        const token = jwtGenerator(user.rows[0].user_id);
+
+        res.json({ token });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send('Server error');
+    }
+});
+
 module.exports = router;
