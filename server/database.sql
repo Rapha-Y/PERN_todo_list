6f7a4030c397eb07d1ee0bba862390c3c5b9547b
Rapha-Y <raph.yuki@gmail.com>
2020-12-28 12:21:18 -0300

    modify todo routes to be user-specific

--- server/database.sql
+++ server/database.sql
@@ -1,13 +1,14 @@
 CREATE DATABASE pernstack;
 
-CREATE TABLE todo(
-    todo_id SERIAL PRIMARY KEY,
-    description VARCHAR(255)
+CREATE TABLE users(
+    user_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_name VARCHAR(255) NOT NULL,
+    user_email VARCHAR(255) NOT NULL UNIQUE,
+    user_password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE users(
-    user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    user_name VARCHAR(255) NOT NULL,
-    user_email VARCHAR(255) NOT NULL,
-    user_password VARCHAR(255) NOT NULL
+CREATE TABLE todos(
+    todo_id SERIAL PRIMARY KEY,
+    description VARCHAR(255) NOT NULL,
+    user_id UUID REFERENCES users(user_id)
 );
--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -2,17 +2,75 @@ const router = require('express').Router();
 const pool = require('../db');
 const authorization = require('../middleware/authorization');
 
+//get user and their todos
 router.get('/', authorization, async (req, res) => {
     try {
         const user = await pool.query(
-            'SELECT user_name FROM users WHERE user_id = $1',
-            [req.user]
+            'SELECT u.user_name, t.todo_id, t.description FROM users AS u LEFT JOIN todos AS t ON u.user_id = t.user_id WHERE u.user_id = $1',
+            [req.user.id]
         );
         
-        res.json(user.rows[0]);
+        res.json(user.rows);
     } catch (error) {
         console.log(error.message);
         res.status(500).json('Server error');
+    }
+});
+
+//create a todo
+router.post('/todos', authorization, async (req, res) => {
+    try {
+        const { description } = req.body;
+        
+        const newTodo = await pool.query(
+            'INSERT INTO todos (description, user_id) VALUES ($1, $2) RETURNING *',
+            [description, req.user.id]
+        );
+
+        res.json(newTodo.rows[0]);
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+//update a todo
+router.put('/todos/:id', authorization, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { description } = req.body;
+
+        const updateTodo = await pool.query(
+            'UPDATE todos SET description = $1 WHERE todo_id = $2 AND user_id = $3 RETURNING *',
+            [description, id, req.user.id]
+        );
+
+        if (updateTodo.rows.length === 0) {
+            return res.json('This todo is not yours');
+        }
+
+        res.json(updateTodo.rows[0]);
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+//delete a todo
+router.delete('/todos/:id', authorization, async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const deleteTodo = await pool.query(
+            'DELETE FROM todos WHERE todo_id = $1 AND user_id = $2 RETURNING *',
+            [id, req.user.id]
+        );
+
+        if (deleteTodo.rows.length === 0) {
+            return res.json('This todo is not yours');
+        }
+
+        res.json(deleteTodo.rows[0]);
+    } catch (error) {
+        console.log(error.message);
     }
 });
 
